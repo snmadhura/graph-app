9c860f281ef031d2434af2efd6756696915ef94f
snmadhura <snmadhura@gmail.com>
2018-06-27 19:58:43 +0530

    not rendering

--- src/container/Chart/Chart.js
+++ src/container/Chart/Chart.js
@@ -25,27 +25,33 @@ class Chart extends Component {
   }
   //}
 
-  updateDataHandler = () => {
-    
-    console.log(this.state.chartData);
-    axios.get('http://www.json-generator.com/api/json/get/cfWnunPFvm?indent=2')
-      .then(response => {
-        
-        let nchartData = [...this.state.chartData];
-        const newdata = Object.values(response.data);
-        console.log(Object.values(response.data));
-        nchartData.datasets = newdata;
-        // console.log(Object.values(chartData.datasets));
-        //console.log(chartData.datasets);
-        //const mapa = Object.values(chartData.datasets);
-        //const result = Object.values(mapa).map((key) => mapa[key]);
-
-        console.log(nchartData);
-        this.setState({chartData:nchartData});
-        console.log(this.state.chartData);
-      })
-  }
   render() {
+
+    this.updateDataHandler = () => {
+    
+      console.log(this.state.chartData);
+      axios.get('http://www.json-generator.com/api/json/get/cgvqxfSAAy?indent=2')
+        .then(response => {
+          
+          let chartData = {...this.state.chartData};
+          //console.log(chartData);
+          //const newdata = response.data;
+          // console.log(Object.values(response.data));
+          chartData.datasets.data = [3, 1, 3, 5, 2, 1];
+          // this.setState(this.state);
+          // console.log(Object.values(chartData.datasets));
+          //console.log(chartData.datasets);
+          //const mapa = Object.values(chartData.datasets);
+          //const result = Object.values(mapa).map((key) => mapa[key]);
+  
+          // console.log(chartData);
+       this.setState({chartData});
+           console.log(this.state.chartData);
+          
+     // console.log(chartData.datasets);
+        })
+    }
+
     return (
       <div className="chart">
         <Bar
