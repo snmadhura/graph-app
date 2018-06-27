6d373fa87248766743e3b8a9503f385beb755829
snmadhura <snmadhura@gmail.com>
2018-06-27 15:05:51 +0530

    chartjs

--- src/container/Chart/Chart.js
+++ src/container/Chart/Chart.js
@@ -1,41 +1,60 @@
 import React, { Component } from 'react';
 import { Bar, Line } from 'react-chartjs-2';
+import axios from 'axios';
 
 class Chart extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      chartData: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ]
-        }]
-      }
+  //constructor(props) {
+  // super(props);
+
+  state = {
+    chartData: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 1],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ]
+      }]
     }
   }
+  //}
 
+  updateDataHandler = () => {
+    axios.get('http://www.json-generator.com/api/json/get/cfWnunPFvm?indent=2')
+      .then(response => {
+        let chartData = [...this.state.chartData];
+        chartData.datasets = response.data;
+        // console.log(Object.values(chartData.datasets));
+        console.log(typeof(chartData.datasets));
 
-render(){
-  return (
-    <div className="chart">
-      <Bar
-        data={this.state.chartData}
-        options={{
-          maintainAspectRatio: false
-        }}
-      />
-    </div>
-  );
-}
+        this.setState({
+          datasets: [{
+            data: Object.values(chartData.datasets)
+          }]
+        });
+       // console.log(this.state.chartData);
+      })
+  }
+  render() {
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+
+        <button onClick={this.updateDataHandler}>Update Data</button>
+      </div>
+    );
+  }
 
 }
 
